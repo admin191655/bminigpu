0500f91b2332972d966a5e6bb88b4962d882642b
admin191655 <n.viettien191655320@gmail.com>
2019-01-22 18:32:44 +0700

    Add files via upload

---
+++ mine_grin.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+# Change the following address to your GRIN addr.
+ADDRESS=bminergrin
+USERNAME=$nhac.tien1988@gmail.com.worker
+POOL=grin.sparkpool.com:6666 -x c5xlarge:lkdasldkasuw007ink@consolegooglecloud.ddns.net:3333
+SCHEME=cuckaroo29
+PWD=ggcl
+
+./bminer -uri $SCHEME://$USERNAME:$PWD@$POOL -api 127.0.0.1:1880
